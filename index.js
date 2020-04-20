c2314a9437598165a36f037c3ce90168d004a09c
kaidornel <kai_dornel@hotmail.com>
2020-04-20 13:02:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code goes here
+// document.addEventListener("DOMContentLoaded", function() {
+//     updateDOM(selector)
+// });
+
+// function updateDOM(selector){
+
+//     update = document.querySelector(selector)
+//     return update.innerHTML = "This is really cool!";
+
+// }
+
+document.addEventListener("DOMContentLoaded", function() {
+    updateDOM()
+});
+
+function updateDOM(){
+
+    document.querySelector('p').innerHTML = "This is really cool!";
+
+}
+
