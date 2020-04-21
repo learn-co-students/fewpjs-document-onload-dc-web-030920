a0fcf8079860f98373fb08c2dba854a9e460e2af
Dawit Gizaw <gizawdawit123.com>
2020-04-21 11:51:32 -0400

    Done.

--- index.js
+++ index.js
@@ -4,7 +4,3 @@ document.addEventListener("DOMContentLoaded", function() {
   });
    
 
-//   document.addEventListener( "DOMContentLoaded", function () {
-//     document.getElementById( "text" ).innerHTML = "This is really cool!";
-//   } );
- 
